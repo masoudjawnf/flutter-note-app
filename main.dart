413e80033f4c394e2bcbc708d52ed48591e35dc9
Sanaz <sanazallahyari1382@gmail.com>
2025-10-31 17:23:42 +0330

    Update main.dart

--- main.dart
+++ main.dart
@@ -4,3 +4,16 @@ import 'note_page.dart';
 void main() {
   runApp(NoteTakingApp());
 }
+
+class NoteTakingApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Note Taking App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: NotePage(),
+    );
+  }
+}
